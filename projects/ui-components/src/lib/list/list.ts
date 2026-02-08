4091f7936bdf193badeba3da6474f966b862c6c5
Marwa996 <marwamoneim221@gmail.com>
2026-02-08 03:12:17 +0200

    feat: implement task list component with initial tasks and styling

--- projects/ui-components/src/lib/list/list.ts
+++ projects/ui-components/src/lib/list/list.ts
@@ -1,15 +1,41 @@
 import { ButtonModule } from 'primeng/button';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CardDetails } from '../../../../models/src/lib/models/card-details.model';
-import { Card } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
+import { Priority, Status } from '../../../../models/src/lib/enums';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'lib-list',
-  imports: [Card, ButtonModule],
+  imports: [ButtonModule, DividerModule, TagModule, Card],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
-export class List {
+export class List implements OnInit {
   @Input() header!: string;
   @Input() tasks!: CardDetails[];
+
+  ngOnInit(): void {
+    this.tasks = [
+      {
+        header: 'Task 1',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam',
+        status: Status.IN_PROGRESS,
+        priority: Priority.HIGH,
+        dueDate: new Date(),
+        assignee: 'John Doe',
+      },
+      {
+        header: 'Task 2',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam',
+        status: Status.DONE,
+        priority: Priority.LOW,
+        dueDate: new Date(),
+        assignee: 'Jane Doe',
+      },
+    ];
+  }
 }
